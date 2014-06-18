70191e448affb81cf2d7e8d5c680b751ce161a5a
Jian Zhou <i@janzhou.org>
2014-06-18 11:02:56 +0800

    weibo.js: config crawler

--- weibo.js
+++ weibo.js
@@ -17,8 +17,9 @@ console.log(auth.authorize_url);
 var since_id = 0;
 var new_since_id = 0;
 //var interval = 1000 * 3600 / 150;
-var interval = 1000 * 3600 / 6;
-var count = 100;
+var crawler = config.get('crawler');
+var interval = crawler.interval;
+var count = crawler.count;
 
 function timeline (param) {
     api.status('home_timeline', config.get('auth'), param, function (err, wbs) {
@@ -45,7 +46,8 @@ function timeline (param) {
             setTimeout(timeline, interval, {'since_id': wbs.since_id, 'count': count}); //本次读结束
             console.log('第一次取，只取一次');
         } else if (since_id < wbs.max_id) { //上次取完成的点为 since_id，
-            setTimeout(timeline, interval, {'max_id': wbs.max_id, 'count': count});
+            //setTimeout(timeline, interval, {'max_id': wbs.max_id, 'count': count});
+            setTimeout(timeline, 1000, {'max_id': wbs.max_id, 'count': count});
             console.log('上次取完的点为' + since_id);
             console.log('这次取到的点为' + wbs.max_id);
         } else {
